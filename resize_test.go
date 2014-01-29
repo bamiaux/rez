0bbf8f9cb8d395806a9f209b43f0e679251388e3
Benoît Amiaux <benoit.amiaux@gmail.com>
2014-01-29 23:22:45 +0100

    Add more tests

--- resize_test.go
+++ resize_test.go
@@ -45,9 +45,7 @@ func writeImage(t *testing.T, name string, img image.Image) {
 	expect(t, err, nil)
 }
 
-func resize(t *testing.T, w, h int, input, output string, filter Filter) {
-	src := readImage(t, input)
-	dst := image.NewYCbCr(image.Rect(0, 0, w, h), image.YCbCrSubsampleRatio420)
+func resize(t *testing.T, dst, src *image.YCbCr, filter Filter) {
 	cfg := AdapterConfig{
 		Input: Descriptor{
 			Width:  src.Rect.Dx(),
@@ -55,8 +53,8 @@ func resize(t *testing.T, w, h int, input, output string, filter Filter) {
 			Ratio:  GetRatio(src.SubsampleRatio),
 		},
 		Output: Descriptor{
-			Width:  w,
-			Height: h,
+			Width:  dst.Rect.Dx(),
+			Height: dst.Rect.Dy(),
 			Ratio:  GetRatio(dst.SubsampleRatio),
 		},
 	}
@@ -64,25 +62,50 @@ func resize(t *testing.T, w, h int, input, output string, filter Filter) {
 	expect(t, err, nil)
 	err = adapter.Resize(dst, src)
 	expect(t, err, nil)
+}
+
+func resizeFiles(t *testing.T, w, h int, input, output string, filter Filter) {
+	src := readImage(t, input)
+	dst := image.NewYCbCr(image.Rect(0, 0, w, h), image.YCbCrSubsampleRatio420)
+	resize(t, dst, src, filter)
 	writeImage(t, output, dst)
 }
 
-func TestResize(t *testing.T) {
-	filters := []Filter{
+var (
+	filters = []Filter{
 		NewBilinearFilter(),
 		NewBicubicFilter(),
 		NewLanczosFilter(3),
 	}
+)
+
+func TestResize(t *testing.T) {
 	sizes := []struct{ w, h int }{
-		{w: 128, h: 128},
-		{w: 256, h: 256},
-		{w: 720, h: 576},
-		{w: 1920, h: 1080},
+		{128, 128},
+		{256, 256},
+		{720, 576},
+		{1920, 1080},
 	}
 	for _, f := range filters {
 		for _, s := range sizes {
 			dst := fmt.Sprintf("testdata/output-%vx%v-%v.png", s.w, s.h, f.Name())
-			resize(t, s.w, s.h, "testdata/lenna.jpg", dst, f)
+			resizeFiles(t, s.w, s.h, "testdata/lenna.jpg", dst, f)
+		}
+	}
+}
+
+func TestBoundaries(t *testing.T) {
+	// test we don't go overread/overwrite even with exotic resolutions
+	src := readImage(t, "testdata/lenna.jpg")
+	for _, f := range filters {
+		tmp := image.NewYCbCr(image.Rect(0, 0, 256, 256), image.YCbCrSubsampleRatio444)
+		resize(t, tmp, src, f)
+		last := tmp.Rect.Dx()
+		for i := 32; i > 0; i >>= 1 {
+			last += i
+			dst := image.NewYCbCr(image.Rect(0, 0, last, last), image.YCbCrSubsampleRatio444)
+			resize(t, dst, tmp, f)
+			resize(t, tmp, dst, f)
 		}
 	}
 }
