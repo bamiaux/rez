0412154a7669d1ccc44362b98a96075c91760ceb
Benoît Amiaux <benoit.amiaux@gmail.com>
2014-02-02 13:31:04 +0100

    Move generated part of scalers into a separate file

--- mkscalers.sh
+++ mkscalers.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 # depends on https://github.com/jimmyfrasche/txt
-cat scalers.go.input | txt -json scalers.go.template > scalers.go
-go fmt scalers.go
+cat fixedscalers.go.input | txt -json fixedscalers.go.template > fixedscalers.go
+go fmt fixedscalers.go
