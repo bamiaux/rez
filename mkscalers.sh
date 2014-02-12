349cc818ade2461fd3a70391378f60f6b744f062
Benoît Amiaux <benoit.amiaux@gmail.com>
2014-02-12 20:56:23 +0100

    Pave the way for vertical asm scalers

--- mkscalers.sh
+++ mkscalers.sh
@@ -2,4 +2,4 @@
 # depends on https://github.com/jimmyfrasche/txt
 cat fixedscalers.go.input | txt -json fixedscalers.go.template > fixedscalers.go
 go fmt fixedscalers.go
-go run gen/gen.go > scalers_amd64.s && echo scalers_amd64.s
+go run gen/hgen.go > hscalers_amd64.s && echo hscalers_amd64.s
